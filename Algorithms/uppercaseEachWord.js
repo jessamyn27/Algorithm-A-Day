c4577eeb339a3806b92bb13af6befd873f131bd9
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2021-10-14 16:31:24 -0500

    commenting out

--- Algorithms/uppercaseEachWord.js
+++ Algorithms/uppercaseEachWord.js
@@ -24,21 +24,21 @@ console.log('CODE EVERYDAY!🦄 👩🏼‍💻 🌈');
 
 
 
-const string = "a man a plan a canal panama";
-const string2 = "hello darkness my old friend";
+// const string = "a man a plan a canal panama";
+// const string2 = "hello darkness my old friend";
 
-const upperCase = function(str) {
-    const splitStr = str.split(" ");
-    const array = [];
+// const upperCase = function(str) {
+//     const splitStr = str.split(" ");
+//     const array = [];
 
-    for (i = 0; i < splitStr.length; i++) {
-        let word = splitStr[i];
-        word[0] = word[0].toUpperCase();
-        const splitWord = word.split("");
-        splitWord[0] = splitWord[0].toUpperCase();
-        splitWord.splice(0, 1, splitWord[0].toUpperCase())
-        array.push(splitWord.join(''));
-    }
-    console.log(array.join(' '));
-};
-upperCase(string2);
+//     for (i = 0; i < splitStr.length; i++) {
+//         let word = splitStr[i];
+//         word[0] = word[0].toUpperCase();
+//         const splitWord = word.split("");
+//         splitWord[0] = splitWord[0].toUpperCase();
+//         splitWord.splice(0, 1, splitWord[0].toUpperCase())
+//         array.push(splitWord.join(''));
+//     }
+//     console.log(array.join(' '));
+// };
+// upperCase(string2);
